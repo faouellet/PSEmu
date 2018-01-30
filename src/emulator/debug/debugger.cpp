84a84496092abe9ca61865c8205e7a20953837d5
faouellet <faouellet@gmail.com>
2018-01-29 19:16:07 -0500

    Adding support for watchpoints

--- src/emulator/debug/debugger.cpp
+++ src/emulator/debug/debugger.cpp
@@ -27,12 +27,68 @@ void Debugger::DeleteBreakpoint(uint32_t address)
     }
 }
 
+// Add a breakpoint that will trigger when the CPU attempts to read from <address>
+void Debugger::AddReadWatch(uint32_t address)
+{
+    if (std::find(m_readWatches.cbegin(), m_readWatches.cend(), address) == m_readWatches.cend())
+    {
+        m_readWatches.push_back(address);
+    }
+}
+
+// Delete read watchpoint at <address>. Does nothing if no watchpoint was set for this address
+void Debugger::DeleteReadWatch(uint32_t address)
+{
+    auto foundIt = std::find(m_readWatches.cbegin(), m_readWatches.cend(), address);
+    if(foundIt != m_readWatches.cend())
+    {
+        m_readWatches.erase(foundIt);
+    }
+}
+
+// Add a breakpoint that will trigger when the CPU attempts to write at <address>
+void Debugger::AddWriteWatch(uint32_t address)
+{
+    if (std::find(m_writeWatches.cbegin(), m_writeWatches.cend(), address) == m_writeWatches.cend())
+    {
+        m_writeWatches.push_back(address);
+    }
+}
+
+// Delete write watchpoint at <address>. Does nothing if no watchpoint was set for this address
+void Debugger::DeleteWriteWatch(uint32_t address)
+{
+    auto foundIt = std::find(m_writeWatches.cbegin(), m_writeWatches.cend(), address);
+    if(foundIt != m_writeWatches.cend())
+    {
+        m_writeWatches.erase(foundIt);
+    }
+}
+
 // Called by the CPU when it's about to execute a new instruction. This function
 // is called before *all* CPU instructions so it needs to be as fast as possible.
 void Debugger::OnPCChange(const R3000A& cpu) const
 {
     auto foundIt = std::find(m_breakpoints.cbegin(), m_breakpoints.cend(), cpu.GetPC());
     if(foundIt != m_breakpoints.cend())
+    {
+        Debug(cpu);
+    }
+}
+
+void Debugger::OnMemoryRead(const R3000A& cpu, uint32_t address) const
+{
+    auto foundIt = std::find(m_readWatches.cbegin(), m_readWatches.cend(), address);
+    if(foundIt != m_readWatches.cend())
+    {
+        Debug(cpu);
+    }
+}
+
+void Debugger::OnMemoryWrite(const R3000A& cpu, uint32_t address) const
+{
+    auto foundIt = std::find(m_writeWatches.cbegin(), m_writeWatches.cend(), address);
+    if(foundIt != m_writeWatches.cend())
     {
         Debug(cpu);
     }
