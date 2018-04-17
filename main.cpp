52734cd8521614a613b519b40b04138ff8780a1c
faouellet <faouellet@gmail.com>
2018-04-17 19:48:27 -0400

    main now show a Qt window

--- main.cpp
+++ main.cpp
@@ -1,23 +1,34 @@
-#include "src/emulator/cpu/r3000a.h"
-#include "src/emulator/memory/bios.h"
+//#include "src/emulator/cpu/r3000a.h"
+//#include "src/emulator/memory/bios.h"
+
+#include <mainwindow.h>
+
+#include <QApplication>
 
 #include <iostream>
 
-using namespace PSEmu;
+//using namespace PSEmu;
 
-int main()
+int main(int argc, char** argv)
 {
-    BIOS bios;
-    bios.Init("SCPH1001.BIN");
+    //BIOS bios;
+    //bios.Init("SCPH1001.BIN");
+    //
+    //Interconnect interconnect{std::move(bios)};
+    //Debugger debugger;
+    //R3000A cpu{std::move(interconnect), std::move(debugger)};
+    //
+    //for(;;)
+    //{
+    //    cpu.Step();
+    //}
+    //
+    //return 0;
 
-    Interconnect interconnect{std::move(bios)};
-    Debugger debugger;
-    R3000A cpu{std::move(interconnect), std::move(debugger)};
+    QApplication app{argc, argv};
 
-    for(;;)
-    {
-        cpu.Step();
-    }
+    MainWindow window;
+    window.show();
 
-    return 0;
+    return app.exec();
 }
